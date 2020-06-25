ad5ad6e3e04e6e23536107494edca0b10711f5af
terry-xiaoyu <506895667@qq.com>
2020-06-25 18:48:08 +0800

    First commit

---
+++ apps/tjson/src/tjson_app.erl
@@ -0,0 +1,18 @@
+%%%-------------------------------------------------------------------
+%% @doc tjson public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(tjson_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    tjson_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
